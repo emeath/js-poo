08f6915470c174f65fbf430f715f4206fcbdda80
emeath <matheus.martins.jeronimo@gmail.com>
2022-05-09 20:37:43 -0300

    Primeira classe

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 class Cliente {
     //campos ou atributos 
-
+    nome;
+    cpf;
+    agencia;
+    saldo;
 }
+
+cliente1 = new Cliente();
+
+cliente1.nome = "Fulano";
+cliente1.cpf = 123123123;
+cliente1.agencia = "0123-AX221";
+cliente1.saldo = 50;
+
+console.log(cliente1);
